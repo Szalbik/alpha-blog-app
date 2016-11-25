1c16eb817b6f5becbfc4e8b92981e914a5bf8b4a
Szalbik <szalbierz.d.k@gmail.com>
2016-11-25 17:46:47 +0100

    Create index for show all articles at one and link all pages together.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,18 @@
 class ArticlesController < ApplicationController
+  def show
+    @article = Article.find(params[:id])
+  end
+
+  def index
+    @articles = Article.all
+  end
+
   def new
     @article = Article.new
+  end
+
+  def edit
+    @article = Article.find(params[:id])
   end
 
   def create
@@ -12,14 +24,6 @@ class ArticlesController < ApplicationController
     else
       render 'new'
     end
-  end
-
-  def show
-    @article = Article.find(params[:id])
-  end
-
-  def edit
-    @article = Article.find(params[:id])
   end
 
   def update
