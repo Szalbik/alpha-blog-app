941505862208533c4d062c012508d63f8e380bfe
Szalbik <szalbierz.d.k@gmail.com>
2016-11-28 19:27:41 +0100

    Add associations and ensure that email is downcase. Put deubger in application layout underneath of footer.

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -12,6 +12,7 @@ class ArticlesController < ApplicationController
   def create
     # render plain: params[:article].inspect  # It show plain hash of title and desc
     @article = Article.new(article_params)
+    @article.user = User.last
     if @article.save
       flash[:success] = "Article was successfully created!"
       redirect_to article_path(@article)
