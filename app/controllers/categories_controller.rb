19b9ff966c510f5cac274770777d53aa5ae8aab5
Szalbik <szalbierz.d.k@gmail.com>
2016-12-07 19:33:54 +0100

    Finish success and fail create category integration test

--- app/controllers/categories_controller.rb
+++ app/controllers/categories_controller.rb
@@ -1,13 +1,28 @@
 class CategoriesController < ApplicationController
   def show
-
+    @category = Category.find(params[:id])
   end
 
   def index
-
+    @categories = Category.all
   end
 
   def new
+    @category = Category.new
+  end
 
+  def create
+    @category = Category.new(category_params)
+    if @category.save
+      flash[:success] = "Yout category was successfully created"
+      redirect_to categories_path
+    else
+      render 'new'
+    end
+  end
+
+  private
+  def category_params
+    params.require(:category).permit(:name)
   end
 end
